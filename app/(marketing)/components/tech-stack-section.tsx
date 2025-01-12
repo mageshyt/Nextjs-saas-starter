aac60fec95b3d3681e62f914a213daba63fe3637
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2025-01-12 22:45:46 +0530

    renamed feature to techstack

--- app/(marketing)/components/tech-stack-section.tsx
+++ app/(marketing)/components/tech-stack-section.tsx
@@ -37,9 +37,9 @@ const TechStackItem = ({ name, description, icon: Icon }: TechStackItemProps) =>
 
 const TechStack= () => {
   return (
-    <section id="features" className="overflow-hidden">
+    <section id="tech-stack" className="overflow-hidden">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2 place-items-center">
           <div className="lg:pr-8 lg:pt-4">
 
             {/* Left Column: Textual content */}
