9ecd92355014f911c79d857dd11c03dcd88ccb62
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2025-01-13 19:25:13 +0530

    :recycle: Refactor sidebar component to remove settings dependency and clean up state management; add .ignore file for dependencies

--- components/dashboard-panel/dashboard-layout.tsx
+++ components/dashboard-panel/dashboard-layout.tsx
@@ -12,14 +12,14 @@ export default function DashboardPanelLayout({
 }) {
   const sidebar = useStore(useSidebar, (x) => x);
   if (!sidebar) return null;
-  const { getOpenState, settings } = sidebar;
+  const { getOpenState  } = sidebar;
   return (
     <>
       <Sidebar />
       <main
         className={cn(
           "min-h-screen bg-zinc-50 dark:bg-zinc-900 transition-[margin-left] ease-in-out duration-300",
-          !settings.disabled && (!getOpenState() ? "lg:ml-[90px]" : "lg:ml-72")
+           !getOpenState() ? "lg:ml-[90px]" : "lg:ml-72"
         )}
       >
         {children}
