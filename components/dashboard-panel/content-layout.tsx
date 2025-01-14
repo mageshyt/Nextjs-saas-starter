65beeaa4fe76c93c4918656de866cc455cef449f
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2025-01-14 12:29:06 +0530

    :art: Enhance ContentLayout component: add optional className prop for customizable styling and remove unused 3D card component

--- components/dashboard-panel/content-layout.tsx
+++ components/dashboard-panel/content-layout.tsx
@@ -1,14 +1,16 @@
+import { cn } from "@/lib/utils";
 import Navbar from "./navbar";
 
 interface ContentLayoutProps {
   children: React.ReactNode;
+  className?: string;
 }
 
-export function ContentLayout({  children }: ContentLayoutProps) {
+export function ContentLayout({ children, className }: ContentLayoutProps) {
   return (
     <div>
       <Navbar />
-      <div className="container mx-auto pt-8 pb-8 px-4">{children}</div>
+      <div className={cn("container mx-auto pt-8 pb-8 px-4", className)}>{children}</div>
     </div>
   );
 }
