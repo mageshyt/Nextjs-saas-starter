d2eb049df38c0c348c66e376ae5707321db89856
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2025-01-18 13:43:16 +0530

    :sparkles: Enhance Prisma client initialization with singleton pattern and acceleration extension for improved performance

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,17 @@
 import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
 declare global {
   // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
+  var prisma: PrismaClientSingleton | undefined;
 }
-export const db = globalThis.prisma || new PrismaClient();
+
+const prismaClientSingleton = () => {
+  return new PrismaClient().$extends(withAccelerate());
+};
+
+export const db = globalThis.prisma || prismaClientSingleton();
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
