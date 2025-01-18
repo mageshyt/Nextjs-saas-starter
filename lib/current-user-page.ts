b018df2c53dd74643a63ca707727d68b9f4d7963
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2025-01-18 13:43:52 +0530

    :recycle: Refactor currentProfilePage function for improved readability and consistency in code formatting

--- lib/current-user-page.ts
+++ lib/current-user-page.ts
@@ -1,20 +1,20 @@
 import { getAuth } from "@clerk/nextjs/server";
-import { NextApiRequest } from "next";
 import { db } from "./db";
 import { NextRequest } from "next/server";
 
 
 
 export const currentProfilePage = async (req: NextRequest) => {
-    const { userId } = getAuth(req);
+  const { userId } = getAuth(req);
+  console.log("User ID:", userId);
 
-    if (!userId) {
-        return null;
-    }
-    const user = await db.user.findUnique({
-        where: {
-            user_id: userId,
-        },
-    });
-    return user;
+  if (!userId) {
+    return null;
+  }
+  const user = await db.user.findUnique({
+    where: {
+      user_id: userId,
+    },
+  });
+  return user;
 };
