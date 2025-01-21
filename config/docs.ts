d35a18a7e885ce1bf6c1e49461618ae3cdec9378
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2025-01-21 20:46:23 +0530

    feat: enhance documentation configuration with icons for improved navigation

--- config/docs.ts
+++ config/docs.ts
@@ -1,4 +1,5 @@
 
+import { Book, Code, CreditCard, Database, FolderCode, Home, Layout, Lock, NotebookPen, Settings, TestTube } from "lucide-react";
 import { DocsConfig } from "types";
 
 export const docsConfig: DocsConfig = {
@@ -19,10 +20,12 @@ export const docsConfig: DocsConfig = {
         {
           title: "Introduction",
           href: "/docs",
+          icon: Home
         },
         {
           title: "Installation",
           href: "/docs/installation",
+          icon: FolderCode
         },
       ],
     },
@@ -32,37 +35,47 @@ export const docsConfig: DocsConfig = {
         {
           title: "Authentification",
           href: "/docs/configuration/authentification",
+          icon: Lock
         },
         {
           title: "Blog",
           href: "/docs/configuration/blog",
+          icon: Book
         },
         {
           title: "Components",
           href: "/docs/configuration/components",
+          icon: Code
+
         },
         {
           title: "Config files",
           href: "/docs/configuration/config-files",
+          icon: Settings
         },
         {
           title: "Database",
           href: "/docs/configuration/database",
+          icon: Database
         },
         {
           title: "Layouts",
           href: "/docs/configuration/layouts",
+          icon: Layout
         },
         {
           title: "Markdown files",
           href: "/docs/configuration/markdown-files",
+          icon: NotebookPen
         },
         {
           title: "Subscriptions",
           href: "/docs/configuration/subscriptions",
-        },{
-          title:"Testing",
-          href:"/docs/configuration/testing"
+          icon: CreditCard
+        }, {
+          title: "Testing",
+          href: "/docs/configuration/testing",
+          icon: TestTube
         }
       ],
     },
